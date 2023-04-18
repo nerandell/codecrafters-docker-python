692224f335d393f2d8035dd162130a000d8324b6
achandaw <achandaw@amazon.com>
2023-04-18 22:47:55 +0100

    Handle subprocess exit code

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"), end="")
     print(completed_process.stderr.decode("utf-8"), end="", file=sys.stderr)
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
