3cd179cb8b5ec51cf8dac2c74c381f27b05d16c4
achandaw <achandaw@amazon.com>
2023-04-18 23:38:52 +0100

    Isolate subprocess

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+import os
+import shutil
 import subprocess
 import sys
 
 
+CHROOT_DIR = "/tmp/chroot"
+
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
+    source = command
+    destination = CHROOT_DIR + command
+    os.makedirs(os.path.dirname(destination), exist_ok=True)
+    shutil.copy(source, destination)
+    os.chroot(CHROOT_DIR)
 
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"), end="")
