d9491bed8ea7ee11e8e9b02def0f59c50f26e52e
achandaw <achandaw@amazon.com>
2023-04-18 22:40:26 +0100

    Route stdout and stderr

--- app/main.py
+++ app/main.py
@@ -3,14 +3,12 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     command = sys.argv[3]
     args = sys.argv[4:]
 
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    print(completed_process.stdout.decode("utf-8"), end="")
+    print(completed_process.stderr.decode("utf-8"), end="", file=sys.stderr)
 
 
 if __name__ == "__main__":
